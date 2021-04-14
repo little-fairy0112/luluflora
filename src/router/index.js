113c8d1a7113d42edf6b03f53b46ed7ece682eb9
洪怡茹 <lovely1516132.cc09@gmail.com>
2021-04-15 00:25:48 +0800

    update 0415

--- src/router/index.js
+++ src/router/index.js
@@ -13,11 +13,13 @@ export default new VueRouter({
       name: "home", //元件呈現的名稱
       path: "/", //對應的虛擬路徑
       component: Home, //對應的元件
-    },
-    {
-      name: "Login", //元件呈現的名稱
-      path: "/login", //對應的虛擬路徑
-      component: Login, //對應的元件
+      children: [
+        {
+          name: "Login", //元件呈現的名稱
+          path: "/login", //對應的虛擬路徑
+          component: Login, //對應的元件
+        },
+      ]
     },
   ]
 });
